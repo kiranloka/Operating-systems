616626efeb9a516f6a6e853b3a8069f2cd2754f4
kiranloka <112564858+kiranloka@users.noreply.github.com>
2023-01-25 13:12:31 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+int main()
+{
+pid_t p;
+printf("before fork\n");
+p=fork();
+if(p==0)
+{
+printf("I am child having id %d\n",getpid());
+printf("My parent's id is %d\n",getppid());
+}
+else{
+printf("My child's id is %d\n",p);
+printf("I am parent having id %d\n",getpid());
+}
+printf("Common\n");
+}
+
+output
+/tmp/OdbhKlavE2.o
+before fork
+My child's id is 23522
+I am parent having id 23521
+Common
+I am child having id 23522
+My parent's id is 1
+Common
