96999a1a6f3d2cdd14ef230ed60948fd603d8c69
kiranloka <112564858+kiranloka@users.noreply.github.com>
2023-01-31 09:27:39 +0530

    Update 40.c

--- 40.c
+++ 40.c
@@ -1,50 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-
-#define SHM_SIZE 1024  /* make it a 1K shared memory segment */
-
-int main(int argc, char *argv[])
-{
-    key_t key;
-    int shmid;
-    char *data;
-    int mode;
-
-    /* make the key: */
-    if ((key = ftok("/tmp/test", 'R')) == -1) {
-        perror("ftok");
-        exit(1);
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <unistd.h> 
+ 
+int main(int argc, char **argv) {
+    int result;
+    
+    char *filename = (char *)malloc(512);
+    if (argc < 2) {
+        strcpy(filename, "/usr/bin/adb");
+    } else {
+        strcpy(filename, argv[1]);
     }
-
-    /* connect to (and possibly create) the segment: */
-    if ((shmid = shmget(key, SHM_SIZE, 0644 | IPC_CREAT)) == -1) {
-        perror("shmget");
-        exit(1);
+    result = access (filename, R_OK); 
+    if ( result == 0 ) {
+        printf("%s is readable\n",filename);    
+    } else {
+        printf("%s is not readable\n",filename);    
     }
-
-    /* attach to the segment to get a pointer to it: */
-    data = shmat(shmid, (void *)0, 0);
-    if (data == (char *)(-1)) {
-        perror("shmat");
-        exit(1);
+ 
+    result = access (filename, W_OK); 
+    if ( result == 0 ) {
+        printf("%s is Writeable\n",filename);   
+    } else {
+        printf("%s is not Writeable\n",filename);   
     }
-
-    /* read or modify the segment, based on the command line: */
-    if (argc == 2) {
-        printf("writing to segment: \"%s\"\n", argv[1]);
-        strncpy(data, argv[1], SHM_SIZE);
-    } else
-        printf("segment contains: \"%s\"\n", data);
-
-    /* detach from the segment: */
-    if (shmdt(data) == -1) {
-        perror("shmdt");
-        exit(1);
+ 
+    result = access (filename, X_OK); 
+    if ( result == 0 ) {
+        printf("%s is executable\n",filename);  
+    } else {
+        printf("%s is not executable\n",filename);  
     }
-
-    return 0;
+    free(filename);
+    return 0;
 }
